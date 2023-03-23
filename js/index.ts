cc9453228c72d6d0aaa5b9484f611e117009510b
logicp <logicp@logicp.ca>
2023-03-23 12:35:31 -0400

    passing result back to native layer

--- js/index.ts
+++ js/index.ts
@@ -2,19 +2,23 @@ import bindings from 'bindings'
 import { IGClient } from "./igclient"
 import type { request } from './util'
 import logger from './logger'
-const GetMessage = bindings('kgramIPC')
+const { poll, OnResult } = bindings('kgramIPC')
 const client     = new IGClient()
 //----------------------------------
-setInterval(() => GetMessage(async (msg: request) =>
+setInterval(() => poll(async (msg: request) =>
 {
   logger.info('Waiting for requests')
   try
   {
     logger.debug({ received: msg })
-    if (await client.post(msg))
+
+    const result = await client.post(msg)
+    if (result)
       logger.info('Successfully posted')
     else
       logger.error('Failed to post')
+
+    OnResult(result);
   }
   catch(e)
   {
