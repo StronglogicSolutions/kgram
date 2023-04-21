d0f6c99ef4e6c2b6ce05fa4b40f19b8165013a95
logicp <logicp@logicp.ca>
2023-04-21 17:17:44 -0400

    using production logging now

--- js/logger.js
+++ js/logger.js
@@ -1,15 +1,18 @@
 
 const pino = require('pino')
 
-const logger = pino({
-  transport: {
+const config = { level: 'trace' }
+
+if (process.env.type !== 'PRODUCTION')
+  config.transport =
+  {
     target: 'pino-pretty',
     options: {
       colorize: true
     }
-  },
-  level: 'trace'
-})
+  }
+
+const logger = pino(config)
 
 module.exports    = logger
 module.exports.lg = module.exports.logger
