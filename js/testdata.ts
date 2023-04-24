1a9d09981ffe3269fd3b684da16d766b7cbc34fd
logicp <logicp@logicp.ca>
2023-04-24 12:06:00 -0400

    reverting this

--- js/testdata.ts
+++ js/testdata.ts
@@ -57,36 +57,76 @@ The DQSH advocates can wink and giggle to one another (or to naive parents) abou
 Enjoy the contradiction of both wanting a performer who demonstrates "no limits or constraints" to the benefit of children, while also asserting that this performer will limit and constrain their sexual expression.`
 
 export const example_posts = [
-  `🧵Gender Queer has been a hot topic, regardless of whether it's the best example of piece of young fiction (made available to a range of children, under varying circumstances, but particularly in libraries for children younger than the book claims to have been intended for).
+`🧵On specializing drag queens for children
 
-  ../`,
+Who are these men? Do they portray themselves as modest females, in order to simply be convincing that they are they opposite sex? No. Not at all.
+.../`,
 
-  `I want to point towards one under-discussed aspect of the book, which is borne from insight that I would credit to @BenjaminABoyce.
+`They are to be extravagant in their expression of the female form. A female that cannot be resisted. A female that won't be ignored. A female that attracts and tantalizes. A female that causes your blood flow to be diverted and your breath to be taken away.
+.../`,
 
-  Benjamin reminded us that much of material which features blatant pornography, including depictions with minors, prepubescent children
+`This is accomplished through attire, which is generally made to be risque, to the makeup, which exaggerates the piercingly feminine and alluring eyes, the flushed and aroused cheeks and the big, smoochy lipstick-laden lips that are waiting to be tasted and kissed.
+.../`,
 
-  ../`,
-  `@BenjaminABoyce (as well as single-digit aged ones) is formulated such as to present to adults as though arousal has been subdued.
+`It is in the sway of hips and the swinging of bottoms that a spanking is hinted because they've been so so naughty. The twerking of the hips demand attention and yearn for the rich pleasures of the senses, which is something the transvestite queen insists is to be celebrated.
+../`,
 
-  Unsexy sex, might be more succinct.
+`You might state that your particular drag queen was toned down, but toned down from what?
 
-  ../`,
-  `@BenjaminABoyce In doing so, it becomes a sanitized means of introducing sexual concepts like bodily pleasure and sex with new partners to young children in a way which some adults feel comfortable excusing.
-  ../`,
-  `Awkward, Means of learning something ELSE (hmm)
+Why must your favourite ritualistic ambassador through which to teach children of the new world be of a type which need be toned down?
+.../`,
 
-  Because of this, books like Gender Queer bear utility of being used as a provocation of conservatives who can then be presented as demonstrating ignorance.
-  ../`,
-  `@BenjaminABoyce Ignorance about the act which is sanitized to become an ideallic ritual, rather than the raw pursuit of carnal pleasure. Digested as sanitized and cerebral. De-eroticized as in collectivist exploit of using it to habituate particular social values.
-  ../`,
-  `@BenjaminABoyce For example, the most gratuitous pictures are presented in light which also allow one to deny the sexuality and claim they, as previously described, are completely benign, and void of lustful gratification.
-  ../`,
-  `@BenjaminABoyce This includes:
-  - Image of females performing oral sex
-  - No real pleasure; uninterested partner vocalizes
-  - performer responds with care/understanding, giving weight to sex as an idealic social ritual to become enlightened as embodied creatures
-  ../`,
-  `This book also chooses characters that are a bit older, but what's missed here is that children are always aspiring to be given the full respect they assume is provided to those that are a bit older.
+`Do you believe all Drag Queen Story Hours will have drag queens that are equally toned down and in the same ways?
+.../`,
 
-  fin`
+`It's quite curious, indeed, that the educators specifically designated or the enlightenment of children are to be ones with a particularly sexual dimension of expression that is, by default, reasonably expected in all other performative scenarios
+.../`,
+
+`(remember, Drag Queens existed before they became storybook companions for kids), save this one. Can we say with certainty that such is the design? Or is this simply an unexpected factor to be mitigated?
+.../`,
+
+`If such a dimension of expression were suppressed, would that not be considered contrary to the purpose of exposing children to an environment of performers who demonstrate the absence of limits and constraints?
+.../`,
+
+`Or, if this is a reasonable detail to attenuate, should it not be expected that such attenuation would take place to varying degrees (a *spectrum*, in fact!)?
+
+What is the preference of the DQSH advocate?
+
+.../`,
+
+`What accountability do they recommend for circumstances where appropriate attenuation is not achieved?
+(Oopsies, now your child is even wiser than you are).
+.../`,
+
+`Because it would be possible to choose any other form of performer or exemplar of a vocation, such as a handicapped athletic champion, daredevil or astronaut.
+.../`,
+
+`But they simply won't do, thus we need to understand why someone would insist upon this particular specialization, and it is simple: these performers, whether or not it is being presented purposefully and explicitly, hold continuous reference to sexual expression
+.../`,
+
+`(no, I don't mean the fact that they are human beings and humans are a sexually reproducing species with biological and social expression of sexuality, I mean that these performers specialize in expressing sexuality through their craft).
+.../`,
+
+`Not only is this known by all adults with any awareness of Drag Queen performance, but these same adults are aware of the ever-present potential for expressions to take place along the sexual dimension before these children.
+.../`,
+
+`It is not the stories being read, nor self-esteem, nor the presentation of identity categories in order to ensure the child will not fear or hate such identities. How do we know?
+.../`,
+
+`Drag isn't to present homosexuality (regardless of whether it may be present in the performance). You don't identify homosexuals by their being in drag. In fact, drag performers have often been heterosexuals who simply developed this particular talent and skillset.
+.../`,
+
+`The drag performer harbours potential for over-the-top sexual expression.
+
+.../`,
+
+`And with that fertile capacity for sexual expression situated right beneath the surface (and sometimes bursting out for all to see)..
+.../`,
+
+`The DQSH advocates can wink and giggle to one another (or to naive parents) about the thin layer of repressed sexual expression which erupt for the children at any moment.
+.../`,
+
+`Enjoy the contradiction of both wanting a performer who demonstrates "no limits or constraints" to the benefit of children, while also asserting that this performer will limit and constrain their sexual expression.
+
+fin`
 ]
