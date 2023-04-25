780e15f80a9522ecbc12208befa49a8156bd1911
logicp <logicp@logicp.ca>
2023-04-24 21:34:36 -0400

    tests

--- test/util.spec.ts
+++ test/util.spec.ts
@@ -39,21 +39,26 @@ describe('Dimensions tests', () =>
   })
 })
 
-// describe('Image tests', () =>
-// {
-//   it('Image is generated from text', async () =>
-//   {
-//     const path = await CreateImage("I know very well that implied, in this critical reflection about the real world, is something made in an unveiling of yet another reality. And, again, I know very well, that to simply substitute an ingenuous perception of reality for a critical one, it is sufficient for the oppressed to liberate themselves. To do so, they need to organize in a revolutionary manner and to transform the real world in a revolutionary manner.\nThe sense of organization requires a conscious action making clear what's unclear in a profound vision of consciousness. It is precisely this  creation of a new reality, prefigured in the revolutionary criticism of the old one, that cannot exhaust the conscientization process")
-//     const file = await ReadFile(path)
-//     assert(file.length > 0, "File is not empty")
-//   })
-// })
+describe('Image tests', () =>
+{
+  it('Image is generated from text', async () =>
+  {
+    const path = await CreateImage("I know very well that implied, in this critical reflection about the real world, is something made in an unveiling of yet another reality. And, again, I know very well, that to simply substitute an ingenuous perception of reality for a critical one, it is sufficient for the oppressed to liberate themselves. To do so, they need to organize in a revolutionary manner and to transform the real world in a revolutionary manner.\nThe sense of organization requires a conscious action making clear what's unclear in a profound vision of consciousness. It is precisely this  creation of a new reality, prefigured in the revolutionary criticism of the old one, that cannot exhaust the conscientization process")
+    const file = await ReadFile(path)
+    assert(file.length > 0, "File is not empty")
+  })
+})
 
 describe('Text tests', () =>
 {
   it('Post generated from thread', async () =>
   {
-    const post = make_post_from_thread(make_reqs(example_posts))
-    assert(post.text.length === 3896, "Post is expected length")
-  })
+    let i = 0
+    for (const chunk of FormatLongPost(make_post_from_thread(make_reqs(example_posts)).text, false))
+    {
+      console.log(`Chunk ${i}`, chunk)
+      await CreateImage(chunk, `Post${i++}.jpg`)
+    }
+    assert(await (await ReadFile('Post0.jpg')).byteLength > 0, "Files were generated")
+  }).timeout(10000)
 })
